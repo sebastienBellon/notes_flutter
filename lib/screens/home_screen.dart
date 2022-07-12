9d1c1cab1a1aa6f09dd52ba5862ed32b5d883b8e
Sébastien Bellon <seb.bellon@protonmail.com>
2022-07-12 21:18:51 +0200

    ✨ add a sliverAppBar

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -10,8 +10,42 @@ class HomeScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return BlocBuilder<AuthBloc, AuthState>(
       builder: (context, state) {
-        return Scaffold();
+        return Scaffold(
+          body: _buildBody(context, state),
+        );
       },
+    );
+  }
+
+  Stack _buildBody(BuildContext context, AuthState authState) {
+    return Stack(
+      children: [
+        CustomScrollView(
+          slivers: <Widget>[
+            SliverAppBar(
+              floating: true,
+              flexibleSpace: const FlexibleSpaceBar(
+                title: Text('Your Notes'),
+              ),
+              leading: IconButton(
+                onPressed: () => authState is Authenticated
+                    ? context.read<AuthBloc>().add(Logout())
+                    : print('Go to Login'),
+                icon: authState is Authenticated
+                    ? const Icon(Icons.exit_to_app)
+                    : const Icon(Icons.account_circle),
+                iconSize: 28.0,
+              ),
+              actions: [
+                IconButton(
+                  onPressed: () => print('Changed theme'),
+                  icon: const Icon(Icons.brightness_4),
+                ),
+              ],
+            )
+          ],
+        ),
+      ],
     );
   }
 }
